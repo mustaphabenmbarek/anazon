47dda837868d9515a2ec32881ed99d336b90158c
Cedric LOMBARDOT <cedric@donkeycode.com>
2023-01-05 12:16:35 +0100

    User listener

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -13,7 +13,8 @@ class UserFixtures extends Fixture
         $user = new User();
         $user->setEmail('admin@admin.com');
         $user->setRoles(['ROLE_ADMIN']);
-        $user->setPassword('$2y$13$QViqjjUzI6QmlJgh4ma3L.2BwH8p357rLA47VHNcW5yeDGPdGSwYa'); // demo
+        $user->plainPassword = 'demo';
+        //$user->setPassword('$2y$13$QViqjjUzI6QmlJgh4ma3L.2BwH8p357rLA47VHNcW5yeDGPdGSwYa'); // demo
         $manager->persist($user);
 
         $manager->flush();
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,6 +27,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
+    public ?string $plainPassword = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $firstname = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -95,5 +100,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
     }
 }
