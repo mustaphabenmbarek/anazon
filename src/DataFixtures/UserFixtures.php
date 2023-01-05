3b8032beb042c4d529442c3d76289ba64bf7b0b7
Cedric LOMBARDOT <cedric@donkeycode.com>
2023-01-05 10:41:29 +0100

    Create admin user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword('$2y$13$QViqjjUzI6QmlJgh4ma3L.2BwH8p357rLA47VHNcW5yeDGPdGSwYa'); // demo
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
