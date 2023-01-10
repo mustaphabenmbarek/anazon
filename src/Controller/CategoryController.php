9117252effdede65b2face0cbb35c8b7270583ce
mustaphabenmbarek <mbenmbarek91@gmail.com>
2023-01-10 17:57:16 +0100

    list et show

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    #[Route('/categories', name: 'app_categories')]
+    public function list(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        return $this->render('category/list.html.twig', [
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
+
+    #[Route('/categories/{id}', name: 'app_category')]
+    public function show(CategoryRepository $categoryRepository, string $id): Response
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $categoryRepository->find($id),
+        ]);
+    }
+    
+    // #[Route('/category', name: 'app_category')]
+    // public function index(): Response
+    // {
+    //     return $this->render('category/index.html.twig', [
+    //         'controller_name' => 'CategoryController',
+    //     ]);
+    // }
 }
